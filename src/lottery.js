025743552ea6953c100302050ddb418214ccc6f8
Shane Colbert <91648839+Shanecolbertgit@users.noreply.github.com>
2022-12-30 01:21:45 +0000

    update

---
+++ src/lottery.js
@@ -0,0 +1,15 @@
+import web3 from './web3';
+
+const address = 'address that returns from teminal with abi';
+
+const abi = 'abi';
+
+
+
+
+
+
+
+
+
+export default new web3.eth.Contract(abi, address);
